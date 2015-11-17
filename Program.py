79de0ab10e4ba4d1297284920302e530757e48f4
lyraf3 <lyraf3@gmail.com>
2015-11-17 23:55:52 +0300

    Update Program.py

--- Program.py
+++ Program.py
@@ -1,6 +1,6 @@
-t = str(input())
+t = str(input(''))
 with open ('sampletext.txt','r') as txt:
     for line in txt:
         line = line.strip()
         if t in line:
-            print(t)
+            print(line)
