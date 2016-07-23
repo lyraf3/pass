9f6565d9b987e0747e88d7f2d0725900a0877a86
sevasan <vchestnov@gamil.com>
2016-07-23 17:48:44 +0200

    Add list of all files in script's directory

--- Program.py
+++ Program.py
@@ -1,5 +1,15 @@
+import os
+
+# Get script directory path
+mypath = os.path.dirname(os.path.realpath(__file__))
+# Get all files in mypath
+onlyfiles = [f for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f))]
+
+print(onlyfiles)
+
+print('Enter a line to search: ')
 t = str(input(''))
-with open ('sampletext.txt','r') as txt:
+with open('sampletext.txt', 'r') as txt:
     for line in txt:
         line = line.strip()
         if t in line:
